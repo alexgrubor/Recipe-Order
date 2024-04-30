ea95a5813979bf24c58ef27915c16dd1de19ac68
Alex <alexgrubor@gmail.com>
2024-04-30 09:10:41 +0200

    "Add type annotation to orders state and update layout with custom image and styled order list"

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -13,7 +13,7 @@ interface Order {
 
 const Order = () => {
   const user = useUser();
-  const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState<Order[]>([]);
 
   useEffect(() => {
     axios.get("/api/order").then((res) => {
@@ -22,22 +22,47 @@ const Order = () => {
   }, []);
 
   return (
-    <div>
-    {orders.map((order: Order) => (
-        <div key={order.id}>
-            <h3>Order ID: {order.id}</h3>
-            <p>Status: {order.status}</p>
-            <p>
-                Items:{" "}
+    <div className="relative text-restaurant-neutral">
+      <div className="absolute inset-0 bg-my-custom-image-4 bg-cover bg-center bg-no-repeat bg-fixed brightness-50"></div>
+
+      <div className="relative z-10 min-h-screen flex pt-32  justify-center items-center px-4 md:px-8 lg:px-16">
+        <div className="flex gap-5 flex-wrap justify-center">
+          {orders.map((order: Order) => (
+            <div
+              key={order.id}
+              className="bg-restaurant-neutral text-restaurant-primary p-3 rounded-md hover:scale-110 transition duration-300 ease-in-out"
+            >
+              <h3>Order ID: {order.id}</h3>
+              <p
+                className={`${
+                  order.status === "pending"
+                    ? "text-restaurant-accent"
+                    : order.status === "delivered"
+                    ? "text-restaurant-secondary"
+                    : "text-restaurant-primary"
+                }`}
+              >
+                Status: {order.status}
+              </p>
+              <ul>
+                Items:
                 {order.items.map((item: CartItem) => (
-                    <li key={item.id}>
-                        {item.name} x {item.quantity}
-                        <Image src={item.imageUrl} alt={item.name} height={200} width={200} />
-                    </li>
+                  <li key={item.id} className="
+                  my-2">
+                    {item.name} x {item.quantity}
+                    <Image
+                      src={item.imageUrl}
+                      alt={item.name}
+                      height={200}
+                      width={200}
+                    />
+                  </li>
                 ))}
-            </p>
+              </ul>
+            </div>
+          ))}
         </div>
-    ))}
+      </div>
     </div>
   );
 };
