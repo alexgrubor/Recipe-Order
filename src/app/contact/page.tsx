1edd357940fbd0d8bcebd547a76185177fa20b18
Alex <alexgrubor@gmail.com>
2024-05-13 12:41:18 +0200

    This commit updates the contact page by changing the text size of some paragraphs, adding padding to the bottom of the container, and the update for submit button color

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,16 +2,16 @@ const page = () => {
   return (
     <div className="relative text-restaurant-neutral">
     <div className="absolute inset-0 bg-my-custom-image-4 bg-cover bg-center bg-no-repeat bg-fixed brightness-50"></div>
-    <div className="relative z-10 min-h-screen flex flex-col justify-center items-center px-4 md:px-8 lg:px-16">
+    <div className="relative z-10 min-h-screen flex flex-col justify-center items-center py-16 px-4 md:px-8 lg:px-16">
       <div className="text-center">
         <h1 className="text-5xl font-bold text-wrap my-3">Contact Us</h1>
-        <p className="text-lg text-balance md:max-w-[60%] mx-auto mt-6">
+        <p className="text-xl text-balance md:max-w-[60%] mx-auto mt-6">
           We would love to hear from you! If you have any questions, comments,
           or suggestions, please feel free to reach out to us. We are here to
           help you in any way we can.
         </p>
         <p className="text-lg text-balance md:max-w-[60%] mx-auto mt-6">
-          You can contact us by phone, email, or by filling out the form
+          You can contact us by email, or by filling out the form
           below. We will do our best to get back to you as soon as possible.
         </p>
         <p className="text-lg text-balance md:max-w-[60%] mx-auto mt-6">
@@ -33,7 +33,7 @@ const page = () => {
             <textarea id="message" name="message" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
           </div>
           <div className="text-center">
-            <button type="submit" className="bg-restaurant-primary hover:bg-primary-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button>
+            <button type="submit" className="bg-restaurant-primary hover:bg-restaurant-accent text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button>
           </div>
         </form>
       </div>
